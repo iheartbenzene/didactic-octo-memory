e586cfe966a43c4f3f00335d8d28da32bf060932
Jevon M <iheartbenzene@gmail.com>
2019-06-07 08:03:20 -0700

    meh

--- python103.py
+++ python103.py
@@ -10,3 +10,11 @@ print(f'{regular_tuple[0]} is a {regular_tuple[1]}')
 User = namedtuple('User', 'name role')
 human = User(name='ted', role = 'checkerboard')
 print(f'{human.name} is a {human.role}')
+
+users = {'user1': 'role1'}
+print(users['user1'])
+try:
+    print(user['user2'])
+except:
+    print('There are no users matching that name or description.')
+
